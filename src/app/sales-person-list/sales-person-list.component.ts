b6a067347d289f9a262a1b0c05accd0d34ede25a
arifur946954 <arif.bd08060@gmail.com>
2023-11-26 23:04:30 +0600

    setup bootstrap inside angular

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -3,15 +3,15 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list.component bootstarp.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
   //create an array of object
   salePersonList:SalesPerson[]=[
     new SalesPerson("Arifur","Rahman","arif@gmail.com",10000),
-    new SalesPerson("Test1","Tester1","test1@gmail.com",10001),
-    new SalesPerson("Test2","Tester2","test3@gmail.com",10002),
+    new SalesPerson("Test1","Tester1","test1@gmail.com",50001),
+    new SalesPerson("Test2","Tester2","test3@gmail.com",60002),
     new SalesPerson("Test3","Tester3","test3@gmail.com",10003)
   ];
 
