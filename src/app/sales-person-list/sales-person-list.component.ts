a98d17c25c5bef7deb169f5699eb25048749c155
arifur946954 <arif.bd08060@gmail.com>
2023-11-26 09:22:34 +0600

    show data for user

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+  //create an array of object
+  salePersonList:SalesPerson[]=[
+    new SalesPerson("Arifur","Rahman","arif@gmail.com",10000),
+    new SalesPerson("Test1","Tester1","test1@gmail.com",10001),
+    new SalesPerson("Test2","Tester2","test3@gmail.com",10002),
+    new SalesPerson("Test3","Tester3","test3@gmail.com",10003)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
